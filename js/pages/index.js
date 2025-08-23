f0449c9ac82b197e8d5cfb8ab31699ba953156eb
joaod <bwb.vega-miranda.j@fondespierre.com>
2025-08-23 21:43:59 +0200

    ajout d'images json dasn la home page

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,1 +1,24 @@
+import { getProducts } from "../shared/api.js"; 
 
+const data = await getProducts();
+const sectionProducts = document.querySelector(".section-div-products");
+
+console.log(data)
+
+export function productsList() {
+    for (let i = 0; i < 8; i++){
+
+    console.log(data[i].title) 
+
+const div = document.createElement('div');
+const pictureProduct = document.createElement("img");
+
+sectionProducts.appendChild(div);
+div.appendChild(pictureProduct)
+
+div.className = "images"
+pictureProduct.src = data[i].thumbnail;
+
+}
+
+}
