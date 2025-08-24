2d9afb0b1048eb9ffdc2954661c3049de32e9eb2
joaod <bwb.vega-miranda.j@fondespierre.com>
2025-08-24 18:48:35 +0200

    essaie integration carousel-pas de css

--- js/pages/index.js
+++ js/pages/index.js
@@ -6,13 +6,17 @@ const sectionProducts = document.querySelector(".section-div-products");
 console.log(data)
 
 export function productsList() {
-    for (let i = 0; i < 8; i++){
+    for (let i = 7 ; i < 15; i++){
 
     console.log(data[i].title) 
 
 const div = document.createElement('div');
 const pictureProduct = document.createElement("img");
+// img.setAttribute('class','active');
+const titleProduct = document.createElement("p");
+titleProduct.textContent = data[i].title;
 
+div.appendChild(titleProduct);
 sectionProducts.appendChild(div);
 div.appendChild(pictureProduct)
 
@@ -20,5 +24,54 @@ div.className = "images"
 pictureProduct.src = data[i].thumbnail;
 
 }
-
 }
+
+
+
+
+const carouselContainer = document.querySelector(".images-carousel");
+
+export function loadCarouselImages(){
+    for ( let i = 1 ; i < 30 ; i++ ){
+        const img = document.createElement("img");
+        img.src =data[i].images[0];
+        img.alt = data[i].title;
+        carouselContainer.appendChild(img);
+
+    const btnRigth = document.querySelector(".carousel-btn-right");
+
+
+    }
+}
+loadCarouselImages();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const carouselContainer = document.querySelector(".images-carousel");
+
+// export function loadCarouselImages(){
+//     for ( let i = 1 ; i < 30 ; i++ ){
+//         const img = document.createElement("img");
+//         img.src =data[i].images[0];
+//         img.alt = data[i].title;
+//         carouselContainer.appendChild(img);
+
+//     const btnRigth = document.querySelector(".carousel-btn-right");
+
+
+//     }
+// }
+// loadCarouselImages();
+
